01133f67b85008815fe779920afb665d1325563f
carlosmca44 <carlosmca4424@gmail.com>
2022-08-19 10:24:11 -0400

    voucher tabpanel

--- src/components/VoucherSection/VoucherSection.tsx
+++ src/components/VoucherSection/VoucherSection.tsx
@@ -1,7 +1,70 @@
+import { Box, Tab, Tabs, Typography } from "@mui/material";
 import React from "react";
 
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role='tabpanel'
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
+
 const VoucherSection = () => {
-  return <>SOME VOUCHERS</>;
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (
+    event: React.SyntheticEvent,
+    newValue: number
+  ) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: "100%" }} mt={2}>
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label='basic tabs example'
+        >
+          <Tab label='Listos' {...a11yProps(0)} />
+          <Tab label='Pendientes a pago' {...a11yProps(1)} />
+          <Tab label='Pagados' {...a11yProps(1)} />
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0}>
+        Pendientes
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Denegadas
+      </TabPanel>
+    </Box>
+  );
 };
 
 export default VoucherSection;
